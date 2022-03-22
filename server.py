0460de4051c16feea008c8f763c029729419afa5
SUVolodin <volodin.stas87@gmail.com>
2022-03-22 21:45:36 +0300

    Вынесение кода шаблона из функции.

--- server.py
+++ server.py
@@ -1,14 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 from weather import weather_by_city
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    title = "Прогноз погоды"
     weather = weather_by_city("Moscow,Russia")
-    if weather:
-        return f"Температура воздуха: {weather['temp_C']}, ощущается как {weather['FeelsLikeC']}"
-    else:
-        return "Сервис погоды временно недоступен."
+    return render_template("index.html", page_title=title, weather=weather)
 if __name__ == "__main__":
     app.run(debug=True)
