e09a6a099ad6f4aa39f4c3073cd7d979d166b648
SUVolodin <volodin.stas87@gmail.com>
2022-03-23 18:28:11 +0300

    Fix

--- webapp/weather.py
+++ webapp/weather.py
@@ -1,9 +1,10 @@
+from flask import current_app
 import requests
 
 def weather_by_city(city_name):
-    weather_url = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
+    weather_url = current_app.config["WEATHER_URL"]
     params = {
-        "key": "5c9e49c87a064f128db203756220403",
+        "key": current_app.config["WEATHER_API_KEY"],
         "q": city_name,
         "format": "json",
         "num_of_days": 1,
