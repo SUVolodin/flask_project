72934640bb083bdd619ce1126d291970f3ce17d8
SUVolodin <volodin.stas87@gmail.com>
2022-03-26 17:20:43 +0300

    Создание модели News и вывод новостей из базы данных.

--- webapp/config.py
+++ webapp/config.py
@@ -1,3 +1,9 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 WEATHER_DEFAULT_CITY = "Moscow,Russia"
 WEATHER_API_KEY = "5c9e49c87a064f128db203756220403"
 WEATHER_URL = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
+SQLALCHEMY_DATABASE_URI ='sqlite:///' + os.path.join(basedir, "..", "webapp.db")
--- webapp/python_news_org.py
+++ webapp/python_news_org.py
@@ -1,5 +1,10 @@
+from datetime import datetime
+
 import requests
 from bs4 import BeautifulSoup
+
+from webapp.model import db, News
+
 
 def get_html(url):
     try:
@@ -9,6 +14,7 @@ def get_html(url):
     except(requests.RequestException, ValueError):
         print("Сетевая ошибка.")
         return False
+
 
 def get_python_news():
     html = get_html("https://www.python.org/blogs/")
@@ -20,10 +26,17 @@ def get_python_news():
             title = news.find("a").text
             url = news.find("a")["href"]
             published = news.find("time").text
-            result_news.append({
-                "title": title,
-                "url": url,
-                "published": published
-            })
-        return result_news
-    return  False
+            try:
+                published = datetime.strptime(published, "%Y-%m-%d")
+            except ValueError:
+                published = datetime.now()
+            save_news(title, url, published)
+
+
+def save_news(title, url, published):
+    news_exists = News.query.filter(News.url == url).count()
+    print(news_exists)
+    if not news_exists:
+        news_news = News(title=title, url=url, published=published)
+        db.session.add(news_news)
+        db.session.commit()
